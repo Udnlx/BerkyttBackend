7826a6f811956dc3c475cc4a504fe5059bcd3c10
Udnlx <Udnlx@yandex.ru>
2026-03-19 20:20:34 +0300

    сделал вывод на странице товара рандомно 4 элемента секции вам может понравится

--- site/api/Products.php
+++ site/api/Products.php
@@ -356,7 +356,7 @@ class Products {
 		}
 
 		$likeit = [];
-		$likeitProducts = $categoryPage->children('template=product, limit=4');
+		$likeitProducts = $categoryPage->children('template=product')->getRandom(4);
 		foreach ($likeitProducts as $likeitProduct) {
 			$img1 = $likeitProduct->images->eq(0);
 			$img2 = $likeitProduct->images->eq(1);
