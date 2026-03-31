90b2cac0bd494dbc405e4a4d66409c59e44a9099
Udnlx <Udnlx@yandex.ru>
2026-03-31 14:20:15 +0300

    доработка точки получения всех заказов пользователя для ЛК

--- site/api/Profile.php
+++ site/api/Profile.php
@@ -13,10 +13,41 @@ class Profile {
 		$getUserOrders = wire('pages')->find('template=order, customer=' . $user->id);
 		$userOrders = [];
 		foreach ($getUserOrders as $order) {
+			$result = [];
+			$totalQuantity = 0; // общее кол-во единиц товара
+			$totalPrice    = 0; // общая сумма
+			foreach ($order->products as $item) {
+				$qty   = (int) $item->quantity;
+				$priceRaw = (string) $item->price;
+  				$price = (float) str_replace([' ', ','], ['', '.'], $priceRaw);
+				$result[] = [
+					'id' => $item->product->id,
+					'name' => $item->product->name,
+					'product' => $item->product->title,
+					'size' => $item->size->title,
+					'sizeRussian' => $item->size->russian_size,
+					'quantity' => $item->quantity,
+					'price' => $item->price,
+				];
+				$totalQuantity += $qty;
+  				$totalPrice    += $price * $qty;
+			}
+			$cart = [
+				'items' => $result,
+				'totalQuantity' => $totalQuantity,
+				'totalPrice' => $totalPrice,
+			];
+
 			$userOrders[] = [
-				'id'  => $order->id,
-				'name'  => $order->name,
-				'title'  => $order->title,
+				'id' => $order->id,
+				'name' => $order->name,
+				'title' => $order->title,
+				'dateOrder' => wireDate('d.m.Y, H:i', $order->created),
+				'cart' => $cart,
+				'address' => $order->address,
+				'deliveryPrice' => $order->delivery_price,
+				'totalSum' => $totalPrice + $order->delivery_price,
+				'orderStatus' => $order->order_status->title,
 			];
 		}
 
